29fa192e48ae24b566151df7cd53c9a9776965a3
Isaac88k <onaiwuisaac@gmail.com>
2023-06-16 12:23:51 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - Determines if a number is positive, negative or zero.
+ * main - Print if a number is positive, negative or zero.
  *
  * Retuen: Always 0 (Success)
  */
