25cc5b99b7d6ad587f66977c2114de89ca844dfa
Isaac88k <onaiwuisaac@gmail.com>
2023-06-20 16:07:58 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 
 /**
- * add - Add two values
- * @nl: The first number to be computed
+ * add - Adds two vaues
+ * @n1: The first number to be computed
  * @n2: The second number to be computed
  * Return: Integer value
  */
