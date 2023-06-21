2737b2130956d24c3aa6ac686f4c642ca6a19afc
Isaac88k <onaiwuisaac@gmail.com>
2023-06-20 18:06:50 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,11 +16,11 @@ void print_times_table(int n)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				k =j * i;
+				k = j * i;
 				if (j == n)
 				{
 					_putchar(k + '0');
-				} else if (k < 10 && j !=0)
+				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
